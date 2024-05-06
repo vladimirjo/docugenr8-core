fe683360569e3f73f4591569e961072eb51d0284
Vladimir Jovanovic <vladimirjo@gmail.com>
2024-05-06 21:51:45 +0200

    add support for pdf

--- src/docugenr8/document.py
+++ src/docugenr8/document.py
@@ -1,8 +1,6 @@
-from docugenr8_shared.dto import Dto
-from docugenr8_shared.dto import DtoFont
-from docugenr8_shared.dto import DtoPage
+from docugenr8_pdf.pdf import Pdf
+from .build_dto import build_dto
 
-from .build_dto import _generate_dto_text_area
 from .font import Font
 from .page import Page
 from .settings import Settings
@@ -32,25 +30,14 @@ class Document:
         self.fonts[font_name] = font
         self.settings.font_current = font_name
 
-    def create_textarea(
-        self, x: float, y: float, width: float, height: float
-    ) -> TextArea:
+    def create_textarea(self, x: float, y: float, width: float, height: float) -> TextArea:
         return TextArea(x, y, width, height, self)
 
-    def build_dto(self) -> Dto:
-        dto = Dto()
-        for font_name, font in self.fonts.items():
-            dto_font = DtoFont(font_name, font.raw_data)
-            dto.fonts.append(dto_font)
-        for page_number, page in enumerate(self.pages):
-            dto_page = DtoPage(page._width, page._height)
-            dto.pages.append(dto_page)
-            for content in page._contents:
-                match content:
-                    case TextArea():
-                        content._build_current_page_fragments(page_number + 1)
-                        content._build_total_pages_fragments(len(self.pages))
-                        dto_page.contents.append(_generate_dto_text_area(content))
-                    case _:
-                        raise TypeError("Invalid content type.")
-        return dto
+    def output_to_bytes(self) -> bytes:
+        pdf = Pdf(build_dto(self))
+        return pdf.output_to_bytes()
+
+    def output_to_file(self, file: str) -> None:
+        b = self.output_to_bytes()
+        with open(file, "wb") as f:
+            f.write(b)
